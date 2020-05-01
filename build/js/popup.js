c062b2485d540c43b31f5ba97fb689bb19f38ff1
Mau224 <46903680+Mau224@users.noreply.github.com>
2020-05-01 18:00:41 +0300

    правки по багам

--- build/js/popup.js
+++ build/js/popup.js
@@ -1,14 +1,18 @@
 'use strict';
 
+var body = document.querySelector('body');
+
 $(document).ready(function($) {
   $('.popup-open').click(function() {
     $('.popup-fade').fadeIn();
     $('#name').focus();
+    body.classList.add('hidden');
     return false;
   });
 
   $('.popup-close').click(function() {
     $(this).parents('.popup-fade').fadeOut();
+    body.classList.remove('hidden');
     return false;
   });
 
@@ -16,12 +20,14 @@ $(document).ready(function($) {
     if (e.keyCode === 27) {
       e.stopPropagation();
       $('.popup-fade').fadeOut();
+      body.classList.remove('hidden');
     }
   });
 
   $('.popup-fade').click(function(e) {
     if ($(e.target).closest('.popup').length === 0) {
       $(this).fadeOut();
+      body.classList.remove('hidden');
     }
   });
 });
